aba5d0c564a497e760029d36d316cb830211eed5
bys <bys@bysdeMBP-2.guest.corp.microsoft.com>
2017-06-21 19:56:32 +0800

    init commit

--- PushForSwift/AppDelegate.swift
+++ PushForSwift/AppDelegate.swift
@@ -7,14 +7,24 @@
 //
 
 import UIKit
+import MobileCenter
+import MobileCenterPush
+import MobileCenterAnalytics
+import MobileCenterCrashes
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, MSPushDelegate{
 
     var window: UIWindow?
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        MSPush.setDelegate(self)
+        MSMobileCenter.start("ed471e5d-961e-4b86-811a-ea05a96e2760", withServices:[
+            MSPush.self,
+            MSAnalytics.self,
+            MSCrashes.self
+            ])
         // Override point for customization after application launch.
         return true
     }
@@ -41,6 +51,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
-
+    func push(_ push: MSPush!, didReceive pushNotification: MSPushNotification!) {
+        var message: String = pushNotification.message
+        for item in pushNotification.customData {
+            message = String(format: "%@\n%@: %@", message, item.key, item.value)
+        }
+        let alert = UIAlertView(title: pushNotification.title, message: message, delegate: self, cancelButtonTitle: "OK")
+        alert.show()
+    }
 }
 
