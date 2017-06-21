efe41ba8d3d03ad5c0634e19d1c9cb0620fb835d
bys <bys@bysdembp-2.guest.corp.microsoft.com>
2017-06-21 17:21:15 +0800

    Initial Commit

---
+++ PushForSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PushForSwift
+//
+//  Created by bys on 21/06/2017.
+//  Copyright © 2017 bys. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
